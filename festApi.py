bac00d4222350e21f13578ad7faa79cbd1fe8df4
ricardozv <ricardosilvanet03@gmail.com>
2022-04-13 19:31:19 -0300

    finish fastApi

--- festApi.py
+++ festApi.py
@@ -1,34 +1,15 @@
-# main.py 
+#festApi.py 
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-from fastapi import FastAPI, status
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine("sqlite:///all.db")
-base = declarative_base()
-
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return "Olá mundo!"
-
-@app.post("/all", status_code=status.HTTP_201_CREATED)
-def create_all():
-    return "Create all"
-
-@app.get("/all/{id}")
-def read_all():
-    return "read item with id"
-
-@app.put("/all/{id}")
-def update(id:int):
-    return "update of item"
-
-@app.delete("/all/{id}")
-def delete(id:int):
-    return "delete item id"
-
-@app.get("/all")
-def readList(id: int):
-    return "read list"
+@app.get("/items/")
+async def create_item(item: Item):
+    return item 
